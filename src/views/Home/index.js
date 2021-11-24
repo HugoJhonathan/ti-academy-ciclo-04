b1d767b83216ab6e649edeb6048a03e79c4c89de
unknown <hugo-jhonathan@hotmail.com>
2021-11-24 15:39:18 -0300

    Ok

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,7 +1,5 @@
 import { Container } from "reactstrap"
 
-import {CadastrarPedido} from '../Pedido/Cadastrar'
-
 export const Home = () => {
     document.title = "TIAcademy Services | Home"
     return (
@@ -10,9 +8,6 @@ export const Home = () => {
                 <div className="d-flex">
                     <div className="m-auto p-2 div1" >
                         <div className="div2"><h1>Home</h1></div>
-                        <div className="div3"><span>asdasd</span></div>
-                        <CadastrarPedido></CadastrarPedido>
-
                     </div>
                 </div>
             </Container>
